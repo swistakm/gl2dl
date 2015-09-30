6d9959588dbb1e3e63731b228d3b1bbda03e5e14
mjaworski <mjaworski@opera.com>
2015-09-30 15:18:01 +0200

    update example

--- example.py
+++ example.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
-from functools import partial
 import sys
-import ctypes
-import numpy as np
 import OpenGL.GL as gl
 import OpenGL.GLUT as glut
-import OpenGL.GLU as glu
-import OpenGL.arrays.vbo as glvbo
+
+import numpy as np
 
 from shader import ShaderProgram
 from lights import Glight
@@ -16,104 +13,29 @@ VS = """
 #version 330 core
 uniform float blue_level;
 
-layout(location = 0) in vec3 position;
-out vec4 g_vertex_color;
+layout(location = 0) in vec2 position;
+out vec4 vertex_color;
 
 void main()
 {
 
-    gl_Position.xy = position.xy;
-    gl_Position.z = 0;
-    gl_Position.w = 1;
+    gl_Position = vec4(position.xy, 0, 1);
 
-    g_vertex_color.rg = (position.xy + 1) / 2.;
-    g_vertex_color.b = blue_level;
+    vertex_color.rg = (position.xy + 1) / 2.;
+    vertex_color.b = blue_level;
 }
 """
-
-# Geometry shader
-GS = """
-#version 330 core
-
-layout(triangles) in;
-layout(triangle_strip, max_vertices = 3) out;
-
-in vec4 g_vertex_color[];
-out vec4 vertex_color;
-
-void main() {
-  for(int i = 0; i < 3; i++) { // You used triangles, so it's always 3
-    vertex_color = g_vertex_color[i];
-    gl_Position = gl_in[i].gl_Position;
-    EmitVertex();
-  }
-  EndPrimitive();
-}
-"""
-
-
-# Geometry shader
-LGS = """
-#version 330 core
-uniform vec3 light_position;
-
-layout(triangles) in;
-layout(triangle_strip, max_vertices = 24) out;
-
-in vec4 g_vertex_color[];
-out vec4 vertex_color;
-
-
-/* extrapolate point on line p1-p2 in distance 'dist' from p2 */
-vec3 extrapolate(vec3 p1, vec3 p2, float dist) {
-    return 2 * (p2 - p1) / distance(p2, p1) + p2;
-}
-
-/* mirror 'point' over given symmetry 'center' */
-vec3 mirror(vec3 point, vec3 center) {
-    return 2 * center - point;
-}
-
-
-void main() {
-    vec4 shadow_color = vec4(1, 1, 1, 1);
-    int k;
-    for (int i=0; i<3; i++){
-        k = i<2 ? i+1 : 0;
-
-        gl_Position = gl_in[i].gl_Position;
-        vertex_color = shadow_color;
-        EmitVertex();
-
-//        gl_Position.xyz = mirror(light_position, gl_in[i].gl_Position.xyz);
-        gl_Position.xyz = extrapolate(light_position, gl_in[i].gl_Position.xyz, 1);
-        vertex_color = shadow_color;
-        EmitVertex();
-
-        gl_Position = gl_in[k].gl_Position;
-        vertex_color = shadow_color;
-        EmitVertex();
-
-//        gl_Position.xyz = mirror(light_position, gl_in[k].gl_Position.xyz);
-        gl_Position.xyz = extrapolate(light_position, gl_in[k].gl_Position.xyz, 1);
-        vertex_color = shadow_color;
-        EmitVertex();
-    }
-}
-"""
-
 
 # Fragment shader
 FS = """
 #version 330 core
 
-out vec4 out_color;
+out lowp vec4 out_color;
 in vec4 vertex_color;
 
 void main()
 {
     out_color = vertex_color;
-    out_color.a = 1;
 }
 """
 
@@ -130,9 +52,7 @@ class GLAPP(object):
         glut.glutCreateWindow('Hello world!')
         glut.glutReshapeWindow(self.width, self.height)
 
-        self.shader_program = ShaderProgram(VS, FS, GS)
-        self.lshader_program = ShaderProgram(VS, FS, LGS)
-
+        self.shader_program = ShaderProgram(VS, FS)
         self.data = data
 
         glut.glutReshapeFunc(self.reshape)
@@ -183,18 +103,12 @@ class GLAPP(object):
         gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, 0)
         # -- end fbo --
 
-        self.light = Glight((1, .5, .5), (0, 0, 0))
+        self.light = Glight((1, .5, .5), (0, 0, 0), data)
 
     def on_mouse_move(self, x, y):
-        self.lshader_program.bind()
-        self.lshader_program['light_position'] = (
-            (float(2 * x) / self.width) - 1,
-            - (float(2 * y) / self.height) + 1,
-            0,
-        )
-        self.light.color = 1, 1, 1
+        self.light.color = 1, 0, 1
         self.light.position = x, self.height - y
-        self.light.radius = 50
+        self.light.radius = 150
 
     def timer(self, fps):
         glut.glutTimerFunc(1000/fps, self.timer, fps)
@@ -203,9 +117,6 @@ class GLAPP(object):
     def loop(self):
         self.shader_program.bind()
         self.shader_program['blue_level'] = 0.9
-
-        self.lshader_program.bind()
-        self.lshader_program['light_position'] = [1, 1, 1]
 
         glut.glutMainLoop()
 
@@ -216,34 +127,19 @@ class GLAPP(object):
 
         try:
             # gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, self.FBO)
+            gl.glClearColor(0, 0, 0, 0)
             gl.glClear(gl.GL_COLOR_BUFFER_BIT)
 
             # draw lights
             self.light.draw()
 
-            gl.glEnable(gl.GL_BLEND)
-            gl.glBlendEquationSeparate(gl.GL_FUNC_REVERSE_SUBTRACT, gl.GL_MIN)
-
-            # draw occluded shadows
-            gl.glBindVertexArray(self.VAO)
-            gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.VBO)
-            gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 0, None)
-            self.lshader_program.bind()
-            gl.glDrawArrays(gl.GL_TRIANGLES, 0, len(self.data))
-
-            # disable blending for now
-            gl.glDisable(gl.GL_BLEND)
-            # gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, 0)
-
             # draw occluders polygons
             gl.glBindVertexArray(self.VAO)
             gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.VBO)
-            gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 0, None)
+            gl.glVertexAttribPointer(0, 2, gl.GL_FLOAT, gl.GL_FALSE, 0, None)
 
             self.shader_program.bind()
             gl.glDrawArrays(gl.GL_TRIANGLES, 0, len(self.data))
-
-
 
         except Exception as err:
             print err
@@ -270,38 +166,35 @@ class GLAPP(object):
 
 
 if __name__ == '__main__':
-    import sys
-    import numpy as np
     data = np.array([
-        [-1, -1, 0],
-        [-1, 1, 0],
-        [1, 1, 0],
+        [-1, -1],
+        [-1, 1],
+        [1, 1],
 
-        [1, -1, 0],
-        [1, 1, 0],
-        [-1, -1, 0],
+        [1, -1],
+        [1, 1],
+        [-1, -1],
 
-        [-3, -3, 0],
-        [-3, -2, 0],
-        [-2, -2, 0],
+        [-3, -3],
+        [-3, -2],
+        [-2, -2],
 
-        [-2, -3, 0],
-        [-2, -2, 0],
-        [-3, -3, 0],
+        [-2, -3],
+        [-2, -2],
+        [-3, -3],
 
-        [3, 3, 0],
-        [3, 2, 0],
-        [2, 2, 0],
+        [3, 3],
+        [3, 2],
+        [2, 2],
 
-        [2, 3, 0],
-        [2, 2, 0],
-        [3, 3, 0],
+        [2, 3],
+        [2, 2],
+        [3, 3],
 
-        [-4, 3, 0],
-        [-3, 5, 0],
-        [-2, 2, 0],
+        [-4, 3],
+        [-3, 5],
+        [-2, 2],
 
     ], dtype=np.float32) / 7.
-
 
     GLAPP(data).loop()
