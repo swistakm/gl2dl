25087b9ef7ab11d37ce900deb0b718e4214dc15f
mjaworski <mjaworski@opera.com>
2016-04-07 17:18:00 +0200

    rect batches WIP

---
+++ examples/example7.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+This example shows that there is need to draw rects in batches!
+"""
+import random
+
+import OpenGL.GLUT as glut
+
+from gl2dl.app import App
+from gl2dl.lights import GLight, ShadowMap
+from gl2dl.primitives import BaseRect, RectBatch
+
+
+class GLAPP(App):
+    def init(self, size, positions):
+        self.rect_batch = RectBatch()
+
+        for pos in positions:
+            self.rect_batch.append([pos, BaseRect(*size)])
+
+        occluders = self.rect_batch.get_triangles()
+        self.light = GLight((1, .5, .5), (0, 0,), occluders)
+
+    def on_mouse_move(self, x, y):
+        self.light.color = 1, 0, 1
+        self.light.position = x, glut.glutGet(glut.GLUT_WINDOW_HEIGHT) - y
+        self.light.radius = 200
+
+    def loop(self):
+        super(GLAPP, self).loop()
+
+    def display(self):
+
+        # # experimental
+        # for row in self.rect_batch:
+        #     pos = row[0]
+        #     row[0] = (
+        #         pos[0] + random.randint(-1, 1),
+        #         pos[1] + random.randint(-1, 1),
+        #     )
+        #
+        # self.light._shadows = ShadowMap(self.rect_batch.get_triangles())
+        # self.light._shadows.position = self.light.position
+
+        try:
+            self.clear()
+            self.light.draw()
+            self.rect_batch.draw()
+
+        except Exception as err:
+            print err
+            exit(1)
+
+if __name__ == '__main__':
+    size = 5, 5
+
+    positions = [
+        (x * 10, y * 10) for x in range(50) for y in range(50)
+    ]
+
+    GLAPP(size=size, positions=positions).loop()
--- gl2dl/primitives.py
+++ gl2dl/primitives.py
@@ -39,7 +39,27 @@ def ortho(width, height, x=0, y=0):
     return matrix
 
 
-class Rect(object):
+class BaseRect(object):
+    """ Base rectangle implementation that do not implement rendering.
+
+    Used only for storing rectangle parameters and providing common API.
+    """
+
+    def __init__(self, width, height, pivot=(0, 0)):
+        self.width = width
+        self.height = height
+        self.pivot = pivot
+
+        self._triangles = rect_triangles(
+            0, 0, width, height
+        ) - np.array(pivot, dtype=np.float32)
+
+    @classmethod
+    def sized(cls, x, y, width, height):
+        return cls(x, y, x + width, y + height)
+
+
+class Rect(BaseRect):
     vertex_code = """
         #version 330 core
 
@@ -68,6 +88,8 @@ class Rect(object):
     """
 
     def __init__(self, width, height, pivot=(0, 0)):
+        super(Rect, self).__init__(width, height, pivot)
+
         self._shader = ShaderProgram(self.vertex_code, self.fragment_code)
 
         self.width = width
@@ -84,10 +106,6 @@ class Rect(object):
         gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.VBO)
         gl.glBufferData(gl.GL_ARRAY_BUFFER, self._triangles.nbytes, self._triangles, gl.GL_STATIC_DRAW)  # noqa
         gl.glEnableVertexAttribArray(0)
-
-    @classmethod
-    def sized(cls, x, y, width, height):
-        return cls(x, y, x + width, y + height)
 
     def draw(self, x, y, color, scale=1.):
         with self._shader as active:
@@ -106,4 +124,84 @@ class Rect(object):
 
             gl.glDrawArrays(gl.GL_TRIANGLES, 0, len(self._triangles))
 
+
+class RectBatch(list):
+    """ Special-case object for rendering multiple rectangles in single shader
+    pass.
+
+    FIXME: maybe deque will be better representation
+    """
+
+    vertex_code = """
+        #version 330 core
+
+        // Input vertex data, different for all executions of this shader.
+        layout(location = 0) in vec2 vertexPosition;
+
+        uniform mat4 model_view_projection;
+        uniform float scale;
+
+        void main(){
+            gl_Position =  model_view_projection * vec4(vertexPosition, 0, 1/scale);
+        }
+    """
+
+    fragment_code = """
+        #version 330 core
+        uniform vec4 color;
+
+        in vec2 vertex_position;
+
+        out lowp vec4 out_color;
+
+        void main(){
+            out_color = color;
+        }
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(RectBatch, self).__init__(*args, **kwargs)
+        self._shader = ShaderProgram(self.vertex_code, self.fragment_code)
+        self.VAO = gl.glGenVertexArrays(1)
+        gl.glBindVertexArray(self.VAO)
+
+        self.VBO = gl.glGenBuffers(1)
+        gl.glEnableVertexAttribArray(0)
+
+
+    def get_triangles(self):
+        triangles = np.array([], dtype=np.float32)
+
+        # fixme: profile this
+        triangles = np.concatenate([
+            rect._triangles + np.array(position, dtype=np.float32)
+            for position, rect in self
+        ])
+
+        return triangles
+
+
+    def draw(self):
+        # fixme: quadratic time performance, improve
+        triangles = self.get_triangles()
+
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.VBO)
+        gl.glBufferData(gl.GL_ARRAY_BUFFER, triangles.nbytes, triangles, gl.GL_STATIC_DRAW)  # noqa
+
+        gl.glBindVertexArray(self.VAO)
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.VBO)
+        gl.glVertexAttribPointer(0, 2, gl.GL_FLOAT, gl.GL_FALSE, 0, None)
+
+        with self._shader as active:
+            active['model_view_projection'] = ortho(
+                glut.glutGet(glut.GLUT_WINDOW_WIDTH),
+                glut.glutGet(glut.GLUT_WINDOW_HEIGHT),
+                0, 0,
+            )
+
+            active['scale'] = 1.
+            active['color'] = [1, 1, 1, 1]
+
+            # draw rect triangles
+            gl.glDrawArrays(gl.GL_TRIANGLES, 0, len(triangles))
 
